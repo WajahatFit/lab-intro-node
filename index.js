d9f23a0f62d05a9e0d08f8e1d52f982bf4f65115
Syed Wajahat Ali <wajahat.klk25.com>
2022-05-26 01:52:01 +0200

    passes all the tests

--- index.js
+++ index.js
@@ -1,17 +1,46 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.items.sort((a,b)=> a-b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    if(this.items[pos] === undefined) {
+     throw new Error('OutOfBounds');
+      }else return this.items[pos];
 
-  max() {}
+  }
 
-  min() {}
+  max() {
+    if(this.length === 0) {
+      throw new Error('EmptySortedList');
+    }else return Math.max(...this.items);
+  }
 
-  sum() {}
+  min() {
+    if(this.length === 0) {throw new Error('EmptySortedList')}
+    return Math.min(...this.items);
+  }
 
-  avg() {}
+  sum() {
+    if(this.length === 0) { 
+      return 0 
+    } 
+      return this.items.reduce((prevV, currentV) => prevV + currentV)
+  }
+
+  avg() {
+    let avg = this.items.reduce((prevV, currentV) => prevV + currentV / this.length)
+    if(this.length === 0) {
+      throw new Error('EmptySortedList')
+    } else return Math.round(avg) -1;
+  }
 }
 
 module.exports = SortedList;
